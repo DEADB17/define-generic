ec569c1ed308fd036bb1e2ed3e0191530f67026e
DEADB17 <DEADB17@gmail.com>
2015-09-12 19:46:42 -0400

    remove built in defaultKey

--- index.js
+++ index.js
@@ -5,11 +5,10 @@
 var owns = Object.prototype.hasOwnProperty;
 
 function defineGeneric(defaultKey, getKey, methods) {
-    var defaultKey2 = defaultKey || '*';
     return function appl(/* arguments */) {
         var fn, key = getKey.apply(getKey, arguments);
         if (owns.call(methods, key)) { fn = methods[key]; }
-        else if (owns.call(methods, defaultKey2)) { fn = methods[defaultKey2]; }
+        else if (owns.call(methods, defaultKey)) { fn = methods[defaultKey]; }
         else { throw new ReferenceError('No such method: ' + key); }
         return fn.apply(fn, arguments);
     };
