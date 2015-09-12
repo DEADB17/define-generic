a805b8707006e9d9866d686f4b264021be59e681
DEADB17 <DEADB17@gmail.com>
2015-09-12 19:55:38 -0400

    make purpose of 1st argument to apply more explicit

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ var owns = Object.prototype.hasOwnProperty;
 
 function defineGeneric(defaultKey, getKey, methods) {
     return function appl(/* arguments */) {
-        var fn, key = getKey.apply(getKey, arguments);
+        var fn, key = getKey.apply(null, arguments);
         if (owns.call(methods, key)) { fn = methods[key]; }
         else if (owns.call(methods, defaultKey)) { fn = methods[defaultKey]; }
         else { throw new ReferenceError('No such method: ' + key); }
-        return fn.apply(fn, arguments);
+        return fn.apply(null, arguments);
     };
 }
 
