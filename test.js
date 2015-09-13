25cbb0f811d685e5e524b7cadc826b38b7eb41e6
DEADB17 <DEADB17@gmail.com>
2015-09-12 20:07:16 -0400

    remove eslint warnings

--- test.js
+++ test.js
@@ -1,19 +1,24 @@
+/*eslint strict:[2,"global"], func-names:0, no-nested-ternary:0*/
+
 'use strict';
 
 var test = require('tape').test;
 var dispatchOn = require('./');
 
-
 test('dispatchOn', function (t) {
-    var selector = function (a, b) {
+    var when, defaults;
+
+    function selector(a, b) {
         return a ? 'A' : b ? 'B' : 'C';
-    };
-    var when = {
+    }
+
+    when = {
         DEFAULT: function () { return 'DEAFAULT method'; },
         A: function () { return 'A method'; },
         B: function () { return 'B method'; }
     };
-    var defaults = dispatchOn('DEFAULT', selector, when);
+
+    defaults = dispatchOn('DEFAULT', selector, when);
 
     t.is(defaults(true), 'A method');
     t.is(defaults(false, true), 'B method');
