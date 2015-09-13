b7513a697237f56b48a74451ee2abc33166aa48a
DEADB17 <DEADB17@gmail.com>
2015-09-12 21:05:31 -0400

    rename definitions

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 'use strict';
 
-function defineGeneric(getKey, methods) {
-    return function appl(/* arguments */) {
-        var key = getKey.apply(null, arguments);
+function defineGeneric(selector, methods) {
+    return function app(/* arguments */) {
+        var key = selector.apply(null, arguments);
         var fn = methods[key];
         if (typeof fn !== 'function') throw new TypeError(key + ' is not a function');
         return fn.apply(null, arguments);
