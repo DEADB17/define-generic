3ebdc895587427a14983153ef0ea7b7123c6bbda
DEADB17 <DEADB17@gmail.com>
2016-01-11 11:04:16 -0500

    refine jsdoc

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 /*eslint strict:0*/
 
+/** @typedef {*} methods */
+
+/** @typedef {function} method */
+
+/**
+ * Function to select a specific method from the set of methods based on the arguments.
+ * @callback getMethod
+ * @arg {methods} methods Data structure to hold the methods.
+ * @arg {*[]} args Arguments array.
+ * @return {method} Method to invoke.
+ */
+
+/**
+ * Function that applies arbitrary arguments to a method.
+ * @callback applyMethod
+ * @arg {method} method Method to use.
+ * @arg {*[]} args Arguments array to apply to the method.
+ * @return {*} Any value.
+ */
+
 /**
  * Creates a generic function
- * @arg {function(methods, arguments[])} getMethod
- * Getter that maps arbitrary arguments to a method in the methods record.
- * @arg {function(method, arguments[])} applyMethod
- * Function that maps arbitrary arguments to a method.
- * @arg {function} defaultMethod
+ * @arg {getMethod} getMethod
+ * Function to select a specific method from the set of methods based on the arguments.
+ * @arg {applyMethod} applyMethod
+ * Function that applies arbitrary arguments to a method.
+ * @arg {method} defaultMethod
  * Function to use when no method is found.
- * @arg {*} methods Data structure to hold the methods.
- * @return {function(...*)} Generic function.
+ * @arg {methods} methods Data structure to hold the methods.
+ * @return {function} Generic function.
  */
 
 module.exports = function create(getMethod, applyMethod, defaultMethod, methods) {
     return function generic(/* arguments */) {
-        var meth = getMethod(methods, arguments);
-        var fn = typeof meth === 'function' ? meth : defaultMethod;
+        var method = getMethod(methods, arguments);
+        var fn = typeof method === 'function' ? method : defaultMethod;
         return applyMethod(fn, arguments);
     };
 };
